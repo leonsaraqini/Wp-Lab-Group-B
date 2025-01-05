c9957224bae896b43270b3c0b4e948c1c52587b5
Leon Saraqini <leonsaraqini@gmail.com>
2025-01-05 20:40:17 +0100

    Create project

---
+++ src/main/java/mk/ukim/finki/lab_group_b/LabGroupBApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.lab_group_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGroupBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGroupBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/lab_group_b/LabGroupBApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.lab_group_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGroupBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
